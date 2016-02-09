fbe126a3bc992d1a7e6bddd49f166ec0c1480c35
Cosmin Ioan <cioan@pih.org>
2016-02-09 13:39:50 -0500

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+install_ansible(){
+	if ! rpm -qa | grep -qw ansible;
+	then
+	    echo "install ansible"
+	    cd /tmp
+		wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+		rpm -ivh epel-release-6-8.noarch.rpm
+		yum repolist
+		# yum -y update
+		yum -y install ansible
+		echo "ansible has been installed"
+	else
+	    echo "ansible is already installed"
+	fi
+}
+
+copy_artifacts(){
+	echo "Copying the files from inventory/$1/"
+	cp -f inventory/$1/local /group_vars/	
+}
+
+install_bahmni_installer(){
+	rm -rf /etc/bahmni-installer
+	yum remove -y bahmni-installer
+	ansible-playbook playbooks/bahmni-installer.yml
+#	yum localinstall -y rpms/bahmni-installer-*.rpm
+#	mkdir -p /etc/bahmni-installer/deployment-artifacts
+}
+
+
+copy_db_dump(){
+	echo "Dropping the openmrs database"
+	ansible bahmni-emr-db -i /etc/bahmni-installer/inventory -m shell -a "mysql -uroot -ppassword openmrs -e 'drop database openmrs'"
+	wget --no-check-certificate $1 -O /etc/bahmni-installer/deployment-artifacts/mysql_dump.sql
+}
+
+deploy(){
+	cd /etc/bahmni-installer && bahmni install inventory
+}
+
+install_ansible
+copy_artifacts
+install_bahmni_installer
