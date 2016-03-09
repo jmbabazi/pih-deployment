6d3ed20dacff91dd6544de7229ec955c0d69cecf
mseaton <mseaton@pih.org>
2016-03-08 21:57:41 -0500

    Spike on what things might look like without ansible

--- config/common.sh
+++ config/common.sh
@@ -2,10 +2,12 @@
 
 # Variables required by Bahmni installer
 
-export BAHMNI_NTP_TIMEZONE="America/New_York"
-export BAHMNI_VERSION="0.80-139"
+export BAHMNI_REPO_URL="https://bahmni-repo.twhosted.com/rpm/bahmni/"
 export BAHMNI_INSTALLER_DIRECTORY="/etc/bahmni-installer"
 export BAHMNI_DEPLOYMENT_ARTIFACTS="$BAHMNI_INSTALLER_DIRECTORY/deployment-artifacts"
+
+export BAHMNI_NTP_TIMEZONE="America/New_York"
+export BAHMNI_VERSION="0.80-139"
 
 export MYSQL_ROOT_PASSWORD="password"
 export OPENMRS_DB_PASSWORD="password"
